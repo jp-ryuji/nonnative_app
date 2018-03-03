c00dbeb3ff1190f0e43834fa64b23180b33e62d5
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-03-03 16:23:20 +0900

    Update .eslinrc.js for react-native

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   "env": {
-    "es6": true
+    "es6": true,
+    "react-native/react-native": true
   },
-  "extends": "eslint:recommended",
+  "extends": ["eslint:recommended", "plugin:react-native/all"],
   "parserOptions": {
     "ecmaFeatures": {
       "experimentalObjectRestSpread": true,
@@ -11,7 +12,8 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react"
+    "react",
+    "react-native"
   ],
   "rules": {
     "indent": [
